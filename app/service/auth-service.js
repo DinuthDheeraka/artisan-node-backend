daa4baf88ca1415bf23de3251e04d23abde5e966
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2024-03-17 12:34:50 +0530

    updated auth-service.js

--- app/service/auth-service.js
+++ app/service/auth-service.js
@@ -1,22 +1,50 @@
+const jwtUtil = require('../util/jwt-util');
+const {findOne} = require('../repo/repo');
 const User = require('../model/User');
+const {compare} = require("bcrypt");
 
-async function persistUser() {
+const login = async ({email, password}) => {
+    console.log("start function login");
     try {
 
-        const newUser = new User({
-            name: "dinuth", email: "dinuth@gmail.com"
-        });
+        /**find user by email*/
+        const user = await findOne(User, {email: email});
 
-        await newUser.save();
+        /***/
+        if (!user) {
+            throw new Error("Invalid email!");
+        }
 
-        return newUser;
+        /**check if password is valid*/
+        const isValidPassword = await compare(password, user.password);
+
+        if (!isValidPassword) {
+            throw new Error("Invalid Password!");
+        }
+
+        /***/
+        if (user && isValidPassword) {
+            /***/
+            const tokens = await jwtUtil.createAccessAndRefreshTokens({username: email});
+            /***/
+            return {
+                success: true,
+                statusCode: 200,
+                user: user,
+                tokens: tokens,
+                message: "Authenticated user successfully!"
+            };
+        }
 
     } catch (err) {
-        console.error("Error persisting user:", err);
-        throw err; // Re-throw the error for handling in the calling layer
+        return {
+            success: false,
+            statusCode: 200,
+            message: err.message
+        };
     }
 }
 
 module.exports = {
-    persistUser,
+    login
 };
