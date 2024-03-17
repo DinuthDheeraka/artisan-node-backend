557dde320deefb473aa83a5046fc6bb5c1d41d77
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2024-03-17 12:34:39 +0530

    updated auth-routes.js

--- app/route/auth/auth-routes.js
+++ app/route/auth/auth-routes.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const authController = require('../../controller/auth-controller');
 
-const router = express.Router();
+const authRouter = express.Router();
 
-router.get('/login', authController.login);
+authRouter.post('/login', authController.login);
 
-module.exports = router;
+module.exports = authRouter;
