cd28a096e1650bf6a3af4ba4f9feab4d976c1d65
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2024-04-01 23:40:20 +0530

    updated user-controller.js

--- app/controller/user-controller.js
+++ app/controller/user-controller.js
@@ -1,10 +1,11 @@
 const userService = require('../service/user-service');
+const {exceptionHandler} = require("../exception/ExceptionHandler");
 
 const saveUser = async (req, res, next) => {
     try {
         return res.status(200).json(await userService.saveUser(req.body));
     } catch (e) {
-        console.log(e);
+        return res.status(200).json(await exceptionHandler(e));
     }
 };
 
